1929f8223a9d288d1d5fd7cb0f7f85aca373710c
Ian Manyama <iannashin@gmail.com>
2018-07-26 10:50:04 +0300

    Ian Manyama | mute auditLog

--- ui/app/common/logging/services/auditLogService.js
+++ ui/app/common/logging/services/auditLogService.js
@@ -35,8 +35,8 @@ angular.module('bahmni.common.logging')
                             params.message = Bahmni.Common.AuditLogEventDetails[eventType].message;
                             params.message = messageParams ? params.message + '~' + JSON.stringify(messageParams) : params.message;
                             params.module = module;
-                            return $http.post(Bahmni.Common.Constants.auditLogUrl, params, {withCredentials: true});
-                        }
+                         return;
+                         //   return $http.post(Bahmni.Common.Constants.auditLogUrl, params, {withCredentials: true});}
                     });
                 };
     }]);
